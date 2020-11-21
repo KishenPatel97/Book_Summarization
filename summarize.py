2e8683183afb58f7525e3c1b3cec2f40d2ed27cc
Jared Kelly <jared.kelly@unt.edu>
2020-11-21 15:24:29 -0600

    added output messages and file capability

--- summarize.py
+++ summarize.py
@@ -1,8 +1,8 @@
-import stanza, nltk, argparse
+import stanza, nltk, argparse, time
 from collections import Counter
 from string import punctuation
 from nltk.corpus import stopwords
-
+t0 = time.time()
 # Parse command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("book", help="file_path to book to be summarized", type=str)
@@ -19,7 +19,8 @@ stanza.download('en')
 
 # Initiate an English LM stanza pipeline
 nlp = stanza.Pipeline('en', processors='tokenize,pos') # we only need tokenize and POS engines
-
+t1 = time.time()
+print("Downloads and Imports Complete, LM setup: {} seconds elapsed".format(round(t1-t0)))
 
 def processBook(fname):
     """ Function to process .txt books, esp. from Gutenberg.
@@ -42,6 +43,7 @@ def buildBookSummary(text, limit):
                      important sentences by TF-IDF
     """
     # FIND KEYWORDS
+    print("Capturing keywords...", end="")
     keywords = []       # initialize list of keywords
     pos_tag = ['PROPN', 'ADJ', 'NOUN', 'VERB']    # list of relevant POS tags
     doc = nlp(text)       # initiate Stanza object from text file
@@ -51,13 +53,17 @@ def buildBookSummary(text, limit):
                 continue
             if word.upos in pos_tag:    # only use words with relevant POS tags: function words aren't key
                 keywords.append(word.text)  # append each keyword token for calculating statistics
-
+    t2 = time.time()
+    print("done. {} seconds elapsed total, {} seconds this task.".format(round(t2-t0), round(t2-t1)))
+    print("Calculating keyword weights...", end="")
     # CALCULATE KEYWORD WEIGHTS
     freq_word = Counter(keywords)       # count the keyword token appearances
     max_freq = Counter(keywords).most_common(1)[0][1]     # capture max token occurence for normalization
     for w in freq_word:
         freq_word[w] = (freq_word[w]/max_freq)      # normalize counts
-
+    t3 = time.time()
+    print("done. {} seconds elapsed total, {} seconds this task.".format(round(t3-t0), round(t3-t2)))
+    print("Calculating sentence weights...", end="")
     # CALCULATE SENTENCE WEIGHTS
     sent_strength={}    # initialize dictionary to store sentence weight
     for sent in doc.sentences:      # iterate through sentence objects
@@ -67,7 +73,9 @@ def buildBookSummary(text, limit):
                     sent_strength[sent.text]+=freq_word[word.text]    # update sentence weight
                 else:      # if this is the first time to see this sentence...
                     sent_strength[sent.text]=freq_word[word.text]     # add the word's weight as the initialization of the sentence's weight
-
+    t4 = time.time()
+    print("done. {} seconds elapsed total, {} seconds this task.".format(round(t4-t0), round(t4-t3)))
+    print("Collecting sentences...", end="")
     # COLLECT BEST SENTENCES BY WEIGHT
     summary = []  # initialize summary storage
     sorted_x = sorted(sent_strength.items(), key=lambda kv: kv[1], reverse=True) # sort the sentences
@@ -77,14 +85,16 @@ def buildBookSummary(text, limit):
         counter += 1      # iterate counter
         if(counter >= limit):     # check for stoppage criteria
             break       # stop at sentence limit
-
+    t5 = time.time()
+    print("done. {} seconds elapsed total, {} seconds this task.".format(round(t5 - t0), round(t5 - t4)))
     return ' '.join(summary)
 
 
 if __name__ == "__main__":
     # Get a sample book from Gutenberg
     # !wget "http://www.gutenberg.org/files/11/11-0.txt"
-    with open("output.txt", 'w') as out_file:
+    print(args.book[12:-4])
+    with open("texts/summaries/whole_summary_"+args.book[12:], 'w') as out_file:
         out_file.write(buildBookSummary(processBook(args.book), args.n_sent))
     # print(buildBookSummary(processBook(args.book), args.n_sent))
 
